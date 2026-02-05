7db1ee4e92a5c1341668769557c7c23e58756069
jin <xarofive@gmail.com>
2026-02-05 15:16:24 +0300

    fix account service post get request

--- account-service/src/main/java/ru/katacademy/bank_app/accountservice/util/DatabaseInitializer.java
+++ account-service/src/main/java/ru/katacademy/bank_app/accountservice/util/DatabaseInitializer.java
@@ -1,43 +1,43 @@
-package ru.katacademy.bank_app.accountservice.util;
-
-import jakarta.annotation.PostConstruct;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.Statement;
-
-/**
- * Автор: Aleksandr Bronnikov
- * <br>
- * Дата: 16.12.2025
- * <p>Автоматическое создание базы данных при запуске сервиса</p>
- */
-@Slf4j
-@Configuration
-public class DatabaseInitializer {
-
-    private final DataSource dataSource;
-
-    @Value("${spring.jpa.properties.hibernate.default_schema}")
-    private String schemaName;
-
-    public DatabaseInitializer(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    @PostConstruct
-    public void init() {
-        log.info("Запуск инициализации схемы для account-service: {}", schemaName);
-        try (Connection connection = dataSource.getConnection();
-             Statement statement = connection.createStatement()) {
-            statement.execute("CREATE SCHEMA IF NOT EXISTS " + schemaName);
-            log.info("Схема {} успешно проверена/создана", schemaName);
-        } catch (Exception e) {
-            log.error("Ошибка при создании схемы БД: ", e);
-            throw new IllegalStateException("Схема БД не создана");
-        }
-    }
-}
+//package ru.katacademy.bank_app.accountservice.util;
+//
+//import jakarta.annotation.PostConstruct;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//import java.sql.Connection;
+//import java.sql.Statement;
+//
+///**
+// * Автор: Aleksandr Bronnikov
+// * <br>
+// * Дата: 16.12.2025
+// * <p>Автоматическое создание базы данных при запуске сервиса</p>
+// */
+//@Slf4j
+//@Configuration
+//public class DatabaseInitializer {
+//
+//    private final DataSource dataSource;
+//
+//    @Value("${spring.jpa.properties.hibernate.default_schema}")
+//    private String schemaName;
+//
+//    public DatabaseInitializer(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+//
+//    @PostConstruct
+//    public void init() {
+//        log.info("Запуск инициализации схемы для account-service: {}", schemaName);
+//        try (Connection connection = dataSource.getConnection();
+//             Statement statement = connection.createStatement()) {
+//            statement.execute("CREATE SCHEMA IF NOT EXISTS " + schemaName);
+//            log.info("Схема {} успешно проверена/создана", schemaName);
+//        } catch (Exception e) {
+//            log.error("Ошибка при создании схемы БД: ", e);
+//            throw new IllegalStateException("Схема БД не создана");
+//        }
+//    }
+//}
